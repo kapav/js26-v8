2928b3ba25fc779c2444f1d8b21733cc4299d3aa
Александр Кочкин <Alexander.Kochkin@artezio.com>
2019-11-09 19:29:42 +0400

    Added fourth example with descendant routes.

--- e4/src/app/phrase-page/phrase-routing.module.ts
+++ e4/src/app/phrase-page/phrase-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import {PhrasePageComponent} from './phrase-page.component'
 import {PhraseListComponent} from '../shared/components/phrase-list/phrase-list.component'
 import {PhraseDetailComponent} from '../shared/components/phrase-detail/phrase-detail.component'
 
@@ -9,8 +10,35 @@ import {PhraseDetailComponent} from '../shared/components/phrase-detail/phrase-d
     // Определение маршрутов для feature-модуля.
     // Метод forRoot должен использоваться только в AppModule.
     RouterModule.forChild([
-      { path: 'phrase', component: PhraseListComponent },
-      { path: 'phrase/:id', component: PhraseDetailComponent }
+      {
+        path: '',
+        redirectTo: '/phrase',
+        pathMatch: 'full'
+      },
+      {
+        // Чтобы компонент отображался в router-outlet'е
+        // PhrasePageComponent'а, а не в AppComponent'е,
+        // необходимо выполнить настройку дочерних маршрутов с
+        // помощью инициализации свойства children.
+        path: 'phrase',
+        component: PhrasePageComponent, // Содержит
+          // <router-outlet>
+        children: [{
+          path: '',
+          component: PhraseListComponent, // Содержит
+            // <router-outlet>
+          children: [
+            {
+              path: ':id',
+              component: PhraseDetailComponent
+            },
+            {
+              path: '',
+              component: PhraseDetailComponent
+            }
+          ]
+        }]
+      }
     ])
   ],
   exports: [RouterModule]
--- e4/src/app/phrase-page/phrase.module.ts
+++ e4/src/app/phrase-page/phrase.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import {PhraseRoutingModule} from './phrase-routing.module'
 
+import { PhrasePageComponent } from './phrase-page.component'
 import {PhraseListComponent} from '../shared/components/phrase-list/phrase-list.component'
-import {PhraseDetailComponent} from '../shared/components/phrase-detail/phrase-detail.component'
+import {PhraseDetailComponent} from '../shared/components/phrase-detail/phrase-detail.component';
 
 @NgModule({
   declarations: [
+    PhrasePageComponent,
     PhraseListComponent,
     PhraseDetailComponent
   ],
